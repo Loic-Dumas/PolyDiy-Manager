970a759d454437c34aa60302a3e3945dac68bd44
Isilin <casa2pir@hotmail.fr>
2016-03-30 23:01:18 +0200

    save

---
+++ src/persistent/ActivityCategory.java
@@ -0,0 +1,11 @@
+package persistent;
+
+import java.util.List;
+
+import common.InterfaceModel;
+
+public abstract class ActivityCategory implements InterfaceModel {
+	protected int idActivityCategory = -1;
+	protected String label = "";
+
+}
