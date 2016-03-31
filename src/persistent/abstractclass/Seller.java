42efa499bd55e878788ef21ee8e58c77d15e71cd
Nassim Vachor <nassim-vachor@hotmail.fr>
2016-03-31 17:40:42 +0200

    ee

--- src/persistent/abstractclass/Seller.java
+++ src/persistent/abstractclass/Seller.java
@@ -34,16 +34,16 @@ public abstract class Seller implements InterfaceModel{
 		this.ID = ID;
 	}
 	/**
-	 * This is a getter of the ID.
+	 * This is a getter of the IDseller.
 	 * @author nassim vachor
 	 * @since 2016-03-21
-	 * return ID
+	 * return IDseller
 	 */
 	public int getId(){
 		return ID;
 	}
 	/**
-	 * The Setter of ID.
+	 * The Setter of IDseller.
 	 * @author nassim vachor
 	 * @since 2016-03-21
 	 * 
@@ -56,37 +56,88 @@ public abstract class Seller implements InterfaceModel{
 	 * This a getter of  id_account.
 	 * @author nassim vachor
 	 * @since 2016-03-21
-	 * return ID
+	 * return IDaccount
 	 */
 	public int getIDaccount() {
 		return this.IDaccount;
 	}
+	/**
+	 * This a setter of  id_account.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 */
 	
 	public void setIDaccount(int IDaccount) {
 		this.IDaccount = IDaccount;
 	}
+	/** 
+	 * This a getter of  nameShop.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * return nameShop
+	 */
 	
 	public String getNameShop() {
 		return nameShop;
 	}
+	/**
+	 * This a setter of  nameShop.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * 
+	 */
 	public void setNameShop(String nameShop) {
 		this.nameShop = nameShop;
 	}
+	/**
+	 * This a getter of  Description.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * return Description
+	 */
 	public String getDescription() {
 		return description;
 	}
+	/**
+	 * This a setter of  Description.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * 
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	/**
+	 * This a getter of  siret.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * return siret
+	 */
 	public String getSiret() {
 		return siret;
 	}
+	/**
+	 * This a setter of  siret.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 */ 
 	public void setSiret(String siret) {
 		this.siret = siret;
 	}
+	/**
+	 * This a getter of  website.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 * return website
+	 */
 	public String getWebsite() {
 		return website;
 	}
+	/**
+	 * This a setter of  website.
+	 * @author nassim vachor
+	 * @since 2016-03-21
+	 */
 	public void setWebsite(String website) {
 		this.website = website;
 	}
